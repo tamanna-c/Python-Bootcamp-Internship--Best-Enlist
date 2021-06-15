9c7c39ab4125f4eec4bcc487dc208054967780f6
tamanna-c <77087383+tamanna-c@users.noreply.github.com>
2021-06-15 22:10:05 +0530

    Add files via upload

---
+++ Day4.py
@@ -0,0 +1,26 @@
+a=b=c=250
+print('data type of a,b,c :',type(a))
+print('divide a value by 5')
+print(a,'/','5','=',a/5)
+print('multiply b value by 35')
+print(b,'*','35','=',b*35)
+print('add c value by 40')
+print(c,'+','40','=',c+40)
+d='Hi12@'
+print('String:',code)
+print('length of the string:',len(d))
+t1='2'
+t2='5'
+d1=d.replace(t1,t2)
+print('New string:',c1)
+a,b=27,4.9
+print('a=',a,'b=',b)
+print('data type of a :',type(a))
+print('data type of b :',type(b))
+a=float(a)
+b=int(b)
+print('After Type Conversion:')
+print('a=',a,'b=',b)
+print('data type of a after type conversion:',type(a))
+print('data type of b after type conversion:',type(b))
+
